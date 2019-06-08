f8c17195a910a12691e8f18e4932331a122f1bf8
Hiren Thacker <hithacker@gmail.com>
2019-06-08 12:41:45 +0530

    0000 | adding userUUID attribute to user pool

--- server/auth.tf
+++ server/auth.tf
@@ -68,6 +68,11 @@ resource "aws_cognito_user_pool" "user_pool" {
     mutable = true
     name = "isOrganisationAdmin"
   }
+  schema {
+    attribute_data_type = "String"
+    mutable = true
+    name = "userUUID"
+  }
 
   tags {
     Name = "${var.environment}-auth-service"
