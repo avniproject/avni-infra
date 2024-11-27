c37361f20226f28bf6306f40843844e09e7c6fb9
himeshr <himeshl@samanvayfoundation.org>
2024-11-27 17:25:28 +0530

    avniproject/avni-infra#34 | Run recent activity for all gamma role users as well

--- reportingSystem/superset/assets/role_permission_upgrade.sql
+++ reportingSystem/superset/assets/role_permission_upgrade.sql
@@ -16,7 +16,7 @@ where role.id not in(
              join ab_view_menu avm on apv.view_menu_id = avm.id
              join ab_permission_view_role apvr on apv.id = apvr.permission_view_id
     where p.name =  'can_recent_activity' and avm.name = 'Superset'
-)and role.name not in ('Public', 'granter', 'sql_lab');
+ )and (role.name not in ('Public', 'granter', 'sql_lab') or (role.name ilike '%gam%' ));
 -- role ids : [17, 18, 20, 22, 23, 24, 26, 27, 29, 38, 39, 25, 44]
 
 
@@ -39,6 +39,6 @@ where role.id not in(
              join ab_view_menu avm on apv.view_menu_id = avm.id
              join ab_permission_view_role apvr on apv.id = apvr.permission_view_id
     where p.name =  'can_read' and avm.name = 'Explore'
-)and role.name ilike '%gam%' ;
+) and role.name ilike '%gam%' ;
 -- role ids : [29, 30, 31, 32, 33, 34, 35, 36, 37, 39]
 
